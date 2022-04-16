80648690834e8b7507ae422003dd4daf7a35db72
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-04-16 14:02:58 -0400

    Add .editorconfig file to solution to have VS auto-format namespaces to file-scoped. Add UseDeveloperExceptionPage() to app HTTP middleware in development mode. Add comments in Program class to make it easier while learning new .NET 6 startup model. Add new Entities directory for project entities. Add a Product entity to the project. Update "launchBrowser" setting in launchSettings.json to false. Minor reformating to the WeatherForecastController example class.

--- ReactECommerceStore.Api/Controllers/WeatherForecastController.cs
+++ ReactECommerceStore.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -13,13 +13,10 @@ public class WeatherForecastController : ControllerBase
     private static readonly string[] Summaries = new[]
     {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController()
     {
-        _logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
--- ReactECommerceStore.Api/Program.cs
+++ ReactECommerceStore.Api/Program.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. (ConfigureServices)
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,9 +13,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. (Configure)
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
