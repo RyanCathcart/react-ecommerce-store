b1a1051c9f270ebb8a8ca658e0babefeca2e487e
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-04-25 14:15:42 -0400

    Add BasketController, BasketDto, and BasketItemDto to web api project.

---
+++ ReactECommerceStore.Api/Controllers/BasketController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactECommerceStore.Api.Data;
+using ReactECommerceStore.Api.DTOs;
+using ReactECommerceStore.Api.Entities;
+
+namespace ReactECommerceStore.Api.Controllers;
+
+public class BasketController : BaseApiController
+{
+    private readonly StoreContext _context;
+    public BasketController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<BasketDto>> GetBasket()
+    {
+        var basket = await RetrieveBasket();
+
+        if (basket == null) return NotFound();
+
+        return new BasketDto
+        {
+            Id = basket.Id,
+            BuyerId = basket.BuyerId,
+            Items = basket.Items.Select(item => new BasketItemDto
+            {
+                ProductId = item.ProductId,
+                Name = item.Product.Name,
+                Price = item.Product.Price,
+                PictureUrl = item.Product.PictureUrl,
+                Type = item.Product.Type,
+                Brand = item.Product.Brand,
+                Quantity = item.Quantity
+            }).ToList()
+        };
+    }
+
+    [HttpPost] // example: api/basket?productId=3&quantity=2
+    public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+    {
+        var basket = await RetrieveBasket();
+
+        if (basket == null) basket = CreateBasket();
+
+        var product = await _context.Products.FindAsync(productId);
+
+        if (product == null) return NotFound();
+
+        basket.AddItem(product, quantity);
+        
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return StatusCode(201);
+
+        return BadRequest(new ProblemDetails{Title = "Problem saving item to basket"});
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+    {
+        // get basket
+        // remove item or reduce quantity
+        // save changes
+        return StatusCode(201);
+    }
+
+    private async Task<Basket> RetrieveBasket()
+    {
+        return await _context.Baskets
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+    }
+
+    private Basket CreateBasket()
+    {
+        var buyerId = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions{IsEssential = true, Expires = DateTime.Now.AddDays(30)};
+        Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+        var basket = new Basket{BuyerId = buyerId};
+        _context.Baskets.Add(basket);
+        return basket;
+    }
+}
--- ReactECommerceStore.Api/Entities/BasketItem.cs
+++ ReactECommerceStore.Api/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ReactECommerceStore.Api.Entities;
 
+[Table("BasketItems")]
 public class BasketItem
 {
     public int Id { get; set; }
@@ -8,4 +11,7 @@ public class BasketItem
     // Navigation properties
     public int ProductId { get; set; }
     public Product Product { get; set; }
+
+    public int BasketId { get; set; }
+    public Basket Basket { get; set; }
 }
