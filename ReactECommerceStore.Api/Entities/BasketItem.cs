43682f3f4e363d7a653c4822cb2695b92f2d0fb5
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-04-25 13:08:06 -0400

    Add Basket and BasketItem classes to web api project. Update StoreContext class to have a Basket DbSet

---
+++ ReactECommerceStore.Api/Entities/BasketItem.cs
@@ -0,0 +1,11 @@
+namespace ReactECommerceStore.Api.Entities;
+
+public class BasketItem
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    
+    // Navigation properties
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+}
