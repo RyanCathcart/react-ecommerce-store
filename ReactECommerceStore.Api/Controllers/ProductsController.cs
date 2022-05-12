519934b529c2b740e431b95733d8acdc26ce323a
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-05-12 14:46:51 -0400

    Update API to paginate the list of products and return a pagination header in the response. Add new API endpoint to retrieve the different brand and types for the filters.

--- ReactECommerceStore.Api/Controllers/ProductsController.cs
+++ ReactECommerceStore.Api/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
-using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReactECommerceStore.Api.Data;
 using ReactECommerceStore.Api.Entities;
+using ReactECommerceStore.Api.Extensions;
+using ReactECommerceStore.Api.RequestHelpers;
 
 namespace ReactECommerceStore.Api.Controllers;
 
@@ -16,9 +19,19 @@ public class ProductsController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams)
     {
-        return await _context.Products.ToListAsync();
+        var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+
+        var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+        Response.AddPaginationHeader(products.MetaData);
+
+        return products;
     }
 
     [HttpGet("{id}")]
@@ -29,5 +42,14 @@ public class ProductsController : BaseApiController
         if (product == null) return NotFound();
 
         return product;
+    }
+
+    [HttpGet("filters")]
+    public async Task<IActionResult> GetFilters()
+    {
+        var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+        var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+        return Ok(new { brands, types });
     }
 }
