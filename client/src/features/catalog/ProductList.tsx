0f8e6c830ad5827cc6498659746a10c5e8ab468b
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-05-14 16:58:56 -0400

    Update agent.ts to use pagination header in the response interceptor. Add AppPaginationProps component to be a reusable pagination component. Add CheckboxButtons component to be a reusable checkbox component. Add RadioButtonGroup component to be a reusable radio button component. Update Catalog component to display product search and product filter menus. Changing the search/filters in the app leaves placeholder cards while new cards load.

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
+import { useAppSelector } from "../../app/store/configureStore";
 import ProductCard from "./ProductCard";
+import ProductCardSkeleton from './ProductCardSkeleton';
 
 interface ProductListProps {
   products: Product[];
 }
 
 export default function ProductList({ products }: ProductListProps) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <Grid item xs={12} sm={6} lg={3} key={product.id}>
-          <ProductCard product={product} />
+        <Grid item xs={12} sm={6} lg={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard product={product} />
+          )}
         </Grid>
       ))}
     </Grid>
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -4,21 +4,50 @@ import {
   createSlice,
 } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
-import { Product } from "../../app/models/product";
+import { MetaData } from "../../app/models/pagination";
+import { Product, ProductParams } from "../../app/models/product";
 import { RootState } from "../../app/store/configureStore";
+
+interface CatalogState {
+  productsLoaded: boolean;
+  filtersLoaded: boolean;
+  status: string;
+  brands: string[];
+  types: string[];
+  productParams: ProductParams;
+  metaData: MetaData | null;
+}
 
 const productsAdapter = createEntityAdapter<Product>();
 
-export const fetchProductsAsync = createAsyncThunk<Product[]>(
-  "catalog/fetchProductsAsync",
-  async (_, thunkAPI) => {
-    try {
-      return await agent.Catalog.list();
-    } catch (error: any) {
-      return thunkAPI.rejectWithValue({ error: error.data });
-    }
+function getAxiosParams(productParams: ProductParams) {
+  const params = new URLSearchParams();
+  params.append("pageNumber", productParams.pageNumber.toString());
+  params.append("pageSize", productParams.pageSize.toString());
+  params.append("orderBy", productParams.orderBy);
+  if (productParams.searchTerm)
+    params.append("searchTerm", productParams.searchTerm);
+  if (productParams.brands.length > 0)
+    params.append("brands", productParams.brands.toString());
+  if (productParams.types.length > 0)
+    params.append("types", productParams.types.toString());
+  return params;
+}
+
+export const fetchProductsAsync = createAsyncThunk<
+  Product[],
+  void,
+  { state: RootState }
+>("catalog/fetchProductsAsync", async (_, thunkAPI) => {
+  const params = getAxiosParams(thunkAPI.getState().catalog.productParams);
+  try {
+    const response = await agent.Catalog.list(params);
+    thunkAPI.dispatch(setMetaData(response.metaData));
+    return response.items;
+  } catch (error: any) {
+    return thunkAPI.rejectWithValue({ error: error.data });
   }
-);
+});
 
 export const fetchProductAsync = createAsyncThunk<Product, number>(
   "catalog/fetchProductAsync",
@@ -37,22 +66,53 @@ export const fetchFiltersAsync = createAsyncThunk(
     try {
       return agent.Catalog.fetchFilters();
     } catch (error: any) {
-      return thunkAPI.rejectWithValue({error: error.data});
+      return thunkAPI.rejectWithValue({ error: error.data });
     }
   }
-)
+);
+
+function initParams() {
+  return {
+    pageNumber: 1,
+    pageSize: 6,
+    orderBy: "name",
+    brands: [],
+    types: [],
+  };
+}
 
 export const catalogSlice = createSlice({
   name: "catalog",
-  initialState: productsAdapter.getInitialState({
+  initialState: productsAdapter.getInitialState<CatalogState>({
     productsLoaded: false,
     filtersLoaded: false,
     status: "idle",
     brands: [],
     types: [],
+    productParams: initParams(),
+    metaData: null,
   }),
-  reducers: {},
-  extraReducers: (builder => {
+  reducers: {
+    setProductParams: (state, action) => {
+      state.productsLoaded = false;
+      state.productParams = {
+        ...state.productParams,
+        ...action.payload,
+        pageNumber: 1,
+      };
+    },
+    setPageNumber: (state, action) => {
+      state.productsLoaded = false;
+      state.productParams = { ...state.productParams, ...action.payload };
+    },
+    setMetaData: (state, action) => {
+      state.metaData = action.payload;
+    },
+    resetProductParams: (state) => {
+      state.productParams = initParams();
+    },
+  },
+  extraReducers: (builder) => {
     // fetchProductsAsync
     builder.addCase(fetchProductsAsync.pending, (state) => {
       state.status = "pendingFetchProducts";
@@ -66,7 +126,6 @@ export const catalogSlice = createSlice({
       console.log(action.payload);
       state.status = "idle";
     });
-
 
     // fetchProductAsync
     builder.addCase(fetchProductAsync.pending, (state) => {
@@ -82,7 +141,6 @@ export const catalogSlice = createSlice({
       state.status = "idle";
     });
 
-
     // fetchFiltersAsync
     builder.addCase(fetchFiltersAsync.pending, (state) => {
       state.status = "pendingFetchFilters";
@@ -97,9 +155,16 @@ export const catalogSlice = createSlice({
       console.log(action.payload);
       state.status = "idle";
     });
-  }),
+  },
 });
 
 export const productSelectors = productsAdapter.getSelectors(
   (state: RootState) => state.catalog
 );
+
+export const {
+  setProductParams,
+  resetProductParams,
+  setMetaData,
+  setPageNumber,
+} = catalogSlice.actions;
