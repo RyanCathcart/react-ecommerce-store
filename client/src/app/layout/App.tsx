cef21a4352a3dfc8fd2e37226bf622e683a03ed0
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-05-27 19:05:20 -0400

    Add reack-hook-form npm package to client project. Update agent.ts to include Account endpoints. Add Login and Register page components. Update App component to have Routes for the Login and Register components

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -23,6 +23,8 @@ import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import { useAppDispatch } from "../store/configureStore";
 import { setBasket } from "../../features/basket/basketSlice";
+import Login from '../../features/account/Login';
+import Register from '../../features/account/Register';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -72,6 +74,8 @@ function App() {
           <Route path="/server-error" component={ServerError} />
           <Route path="/basket" component={BasketPage} />
           <Route path="/checkout" component={CheckoutPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <Route component={NotFound} />
         </Switch>
       </Container>
