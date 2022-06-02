9294e95fd159b713b621ed68436aea0cad5b1f31
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-06-02 17:45:39 -0400

    Add OrderDto and OrderItemDto classes to API project to control data being sent from the web API. Add OrderExtensions.cs to implement an Order to OrderDto conversion method, which the OrdersController class makes use of

---
+++ ReactECommerceStore.Api/DTOs/OrderDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using ReactECommerceStore.Api.Entities.OrderAggregate;
+
+namespace ReactECommerceStore.Api.DTOs;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public ShippingAddress ShippingAddress { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; }
+    public long Subtotal { get; set; }
+    public long DeliveryFee { get; set; }
+    public string OrderStatus { get; set; }
+    public long Total { get; set; }
+}
