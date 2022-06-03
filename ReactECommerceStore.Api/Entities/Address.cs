72fd9b48c7bd86840921b9e52bfc79db8d21dc8e
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-06-03 14:50:47 -0400

    Add AppTextInput react component to simplify the boilerplate code that was being used in the checkout form, and to utilize the features of the react-hook-form library

--- ReactECommerceStore.Api/Entities/Address.cs
+++ ReactECommerceStore.Api/Entities/Address.cs
@@ -6,7 +6,7 @@ public class Address
     public string Address1 { get; set; }
     public string Address2 { get; set; }
     public string City { get; set; }
-    public string  State { get; set; }
+    public string State { get; set; }
     public string Zip { get; set; }
-    public string  Country { get; set; }
+    public string Country { get; set; }
 }
