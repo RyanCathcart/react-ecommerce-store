279a9452643473b1800921b23fa8d572c8daf397
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-06-11 15:19:41 -0400

    Update CheckoutPage component with a submitOrder function that submits the user's order to Stripe. Update the basket model to include the stripe paymentIntentId and stripe clientSecret to support the new submit order functionality. Update OrdersController in the web api project to create orders with the paymentIntentId

--- ReactECommerceStore.Api/Controllers/OrdersController.cs
+++ ReactECommerceStore.Api/Controllers/OrdersController.cs
@@ -78,7 +78,8 @@ public class OrdersController : BaseApiController
             BuyerId = User.Identity.Name,
             ShippingAddress = orderDto.ShippingAddress,
             Subtotal = subtotal,
-            DeliveryFee = deliveryFee
+            DeliveryFee = deliveryFee,
+            PaymentIntentId = basket.PaymentIntentId
         };
 
         _context.Orders.Add(order);
