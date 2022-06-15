b30a6817a6552e026c875bd96dc27e177ad07290
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-06-15 16:20:53 -0400

    Update ProductsController to include an UpdateProduct and DeleteProduct endpoint. Add UpdateProductDto specifically for the UpdateProduct endpoint, and added a corresponding mapping profile for it in the MappingProfiles class

--- ReactECommerceStore.Api/Controllers/ProductsController.cs
+++ ReactECommerceStore.Api/Controllers/ProductsController.cs
@@ -71,4 +71,38 @@ public class ProductsController : BaseApiController
 
         return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
     }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut]
+    public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+    {
+        var product = await _context.Products.FindAsync(productDto.Id);
+
+        if (product == null) return NotFound();
+
+        _mapper.Map(productDto, product);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return NoContent();
+
+        return BadRequest(new ProblemDetails { Title = "Problem updating product" });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        _context.Products.Remove(product);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "Problem deleting product" });
+    }
 }
--- ReactECommerceStore.Api/RequestHelpers/MappingProfiles.cs
+++ ReactECommerceStore.Api/RequestHelpers/MappingProfiles.cs
@@ -9,5 +9,6 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<CreateProductDto, Product>();
+        CreateMap<UpdateProductDto, Product>();
     }
 }
