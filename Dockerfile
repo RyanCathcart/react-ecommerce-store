21c7c5108e4deafc169356534f422c1cb6711c09
Ryan Cathcart <ryanwcathcart@gmail.com>
2023-09-13 23:04:46 -0400

    Update Program.cs to now build a DB connection string that Railway will accept Add Dockerfile and .dockerignore to project for Railway to accept Update appsetting.Development.json to be more consistent with other projects I wrote

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build-env
+WORKDIR /app
+
+# Copy .csproj and restore as distinct layers
+COPY "ReactECommerceStore.sln" "ReactECommerceStore.sln"
+COPY "ReactECommerceStore.Api/ReactECommerceStore.Api.csproj" "ReactECommerceStore.Api/ReactECommerceStore.Api.csproj"
+
+RUN dotnet restore "ReactECommerceStore.sln"
+
+# Copy everything else, and build
+
+COPY . .
+WORKDIR /app
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:7.0
+WORKDIR /app
+COPY --from=buildenv /app/out .
+ENTRYPOINT [ "dotnet", "ReactECommerceStore.Api.dll" ]
--- ReactECommerceStore.Api/Program.cs
+++ ReactECommerceStore.Api/Program.cs
@@ -59,18 +59,24 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     else
     {
         // Use connection string provided at runtime by Heroku.
-        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        // var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
         // Parse connection URL to connection string for Npgsql
-        connUrl = connUrl.Replace("postgres://", string.Empty);
-        var pgUserPass = connUrl.Split("@")[0];
-        var pgHostPortDb = connUrl.Split("@")[1];
-        var pgHostPort = pgHostPortDb.Split("/")[0];
-        var pgDb = pgHostPortDb.Split("/")[1];
-        var pgUser = pgUserPass.Split(":")[0];
-        var pgPass = pgUserPass.Split(":")[1];
-        var pgHost = pgHostPort.Split(":")[0];
-        var pgPort = pgHostPort.Split(":")[1];
+        // connUrl = connUrl.Replace("postgres://", string.Empty);
+        // var pgUserPass = connUrl.Split("@")[0];
+        // var pgHostPortDb = connUrl.Split("@")[1];
+        // var pgHostPort = pgHostPortDb.Split("/")[0];
+        // var pgDb = pgHostPortDb.Split("/")[1];
+        // var pgUser = pgUserPass.Split(":")[0];
+        // var pgPass = pgUserPass.Split(":")[1];
+        // var pgHost = pgHostPort.Split(":")[0];
+        // var pgPort = pgHostPort.Split(":")[1];
+
+        var pgDb = Environment.GetEnvironmentVariable("PGDATABASE");
+        var pgUser = Environment.GetEnvironmentVariable("PGUSER");
+        var pgPass = Environment.GetEnvironmentVariable("PGPASSWORD");
+        var pgHost = Environment.GetEnvironmentVariable("PGHOST");
+        var pgPort = Environment.GetEnvironmentVariable("PGPORT");
 
         connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
     }
